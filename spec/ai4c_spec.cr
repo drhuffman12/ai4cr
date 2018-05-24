7de0cda7c3c94576c951b1ff75da54fb3a42bfd5
Daniel Huffman <drhuffman12@yahoo.com>
2018-05-24 11:55:33 -0400

    add forced failure to test CI system

--- spec/ai4c_spec.cr
+++ spec/ai4c_spec.cr
@@ -1,9 +1,9 @@
 require "./spec_helper"
 
-# describe Ai4cr do
-#   # TODO: Write tests
+describe Ai4cr do
+  # TODO: Write tests
 
-#   it "works" do
-#     false.should eq(true)
-#   end
-# end
+  it "forced failure to test CI system" do
+    false.should eq(true)
+  end
+end
