87d05c6771dc3ca6c12fd7ef44b591f4358a813b
Daniel Huffman <drhuffman12@gmail.com>
2019-05-10 17:17:49 -0400

    re-show ci badge (and force failed test to confirm ci failure)

--- spec/ci_badge_spec.cr
+++ spec/ci_badge_spec.cr
@@ -2,8 +2,8 @@ require "./spec_helper"
 
 # FIX (for README): [![CircleCI](https://circleci.com/gh/drhuffman12/ai4cr.svg?style=svg)](https://circleci.com/gh/drhuffman12/ai4cr)
 
-# describe Ai4cr do
-#   it "ci badge check" do
-#     false.should eq(true)
-#   end
-# end
+describe Ai4cr do
+  it "ci badge check" do
+    false.should eq(true)
+  end
+end
