a4f52ed7135909943477ca3e2344366e417f5f2f
Daniel Huffman <drhuffman12@gmail.com>
2019-05-10 02:05:29 -0400

    remove ci status badge

--- spec/ci_badge_spec.cr
+++ spec/ci_badge_spec.cr
@@ -1,7 +1,9 @@
 require "./spec_helper"
 
-describe Ai4cr do
-  it "ci badge check" do
-    false.should eq(true)
-  end
-end
+# FIX (for README): [![CircleCI](https://circleci.com/gh/drhuffman12/ai4cr.svg?style=svg)](https://circleci.com/gh/drhuffman12/ai4cr)
+
+# describe Ai4cr do
+#   it "ci badge check" do
+#     false.should eq(true)
+#   end
+# end
