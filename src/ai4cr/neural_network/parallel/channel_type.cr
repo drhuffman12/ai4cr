ff3ee9c49354824ca1db89b909688042e4558013
Daniel Huffman <drhuffman12@gmail.com>
2019-12-26 04:11:29 -0500

    add enum ChannelType and struct Coord

---
+++ src/ai4cr/neural_network/parallel/channel_type.cr
@@ -0,0 +1,30 @@
+
+require "json"
+
+module Ai4cr
+  module NeuralNetwork
+    module Parallel
+      enum ChannelType
+        Input  = -1
+        Past   =  0
+        Local  =  1
+        Future =  2
+        Combo  =  3 # aka Output
+
+        def is_input?
+          self == ChannelType::Input
+        end
+
+        def is_hidden?
+          self == !is_input? && !is_output?
+        end
+
+        def is_output?
+          self == ChannelType::Combo
+        end
+      end
+    end
+  end
+end
+
+
