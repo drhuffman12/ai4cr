0375e1d1109914c8262d77d8f5a6e93e495e9716
Daniel Huffman <drhuffman12@gmail.com>
2020-02-17 23:29:49 -0500

    add props/initialziers

--- src/ai4cr/neural_network/cmn/connected_net_set/chain_v2.cr
+++ src/ai4cr/neural_network/cmn/connected_net_set/chain_v2.cr
@@ -16,10 +16,9 @@ module Ai4cr
   end
 end
 
-
-ne = Ai4cr::NeuralNetwork::Cmn::MiniNet::ExpV2.new(foo: 1, bar: 7)
-nr = Ai4cr::NeuralNetwork::Cmn::MiniNet::ReluV2.new(foo: 2, bar: 8)
-nt = Ai4cr::NeuralNetwork::Cmn::MiniNet::TanhV2.new(foo: 3, bar: 9)
+ne = Ai4cr::NeuralNetwork::Cmn::MiniNet::ExpV2.new(height: 1, width: 7)
+nr = Ai4cr::NeuralNetwork::Cmn::MiniNet::ReluV2.new(height: 2, width: 8)
+nt = Ai4cr::NeuralNetwork::Cmn::MiniNet::TanhV2.new(height: 3, width: 9)
 
 arr = [ne,nr,nt]
 cv2 = Ai4cr::NeuralNetwork::Cmn::ConnectedNetSet::ChainV2.new(net_set: arr)
