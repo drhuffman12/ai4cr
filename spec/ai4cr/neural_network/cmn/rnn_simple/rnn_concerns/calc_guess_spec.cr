b0d1e51e91f737e5baa590110c4a24141d8c2aaf
drhuffman12 <drhuffman12@gmail.com>
2020-12-31 19:56:34 -0500

    drhuffman12 prep initial tests for 'Ai4cr::NeuralNetwork::Cmn::RnnConcerns::CalcGuess'

---
+++ spec/ai4cr/neural_network/cmn/rnn_simple/rnn_concerns/calc_guess_spec.cr
@@ -0,0 +1,51 @@
+require "./../../../../../spec_helper"
+require "./../../../../../spectator_helper"
+
+# Spectator.describe Ai4cr::NeuralNetwork::Cmn::RnnSimple do
+Spectator.describe Ai4cr::NeuralNetwork::Cmn::RnnConcerns::CalcGuess do
+  describe "#eval" do
+    let(rnn_simple) { Ai4cr::NeuralNetwork::Cmn::RnnSimple.new }
+
+    let(expected_outputs_guessed_before) { [[0.0], [0.0]] }
+    let(expected_outputs_guessed_after) { [[0.0], [0.1]] }
+    
+    let(input_set_given) {
+      [
+        [0.1, 0.2],
+        [0.3, 0.4]
+      ]
+    }
+    let(guess) { rnn_simple.eval(input_set_given) }
+
+    context "before" do
+      it "outputs_guessed start off all zero's" do
+        expect(rnn_simple.outputs_guessed).to eq(expected_outputs_guessed_before)
+      end
+    end
+
+    context "during" do
+      it "calls 'step_load_inputs'"do
+        expect(rnn_simple).to receive(:step_load_inputs).with(input_set_given)
+
+        rnn_simple.eval(input_set_given)
+      end
+
+      it "calls 'step_calc_forward'"do
+        expect(rnn_simple).to receive(:step_calc_forward).with(input_set_given)
+
+        rnn_simple.eval(input_set_given)
+      end
+    end
+
+    context "after" do
+      it "returns exprected outputs" do
+        expect(rnn_simple.outputs_guessed).to eq(expected_outputs_guessed_before)
+
+        # guess = 
+        rnn_simple.eval(input_set_given)
+
+        expect(rnn_simple.outputs_guessed).to eq(expected_outputs_guessed_after)
+      end
+    end
+  end
+end
--- src/ai4cr/neural_network/cmn/rnn_concerns/calc_guess.cr
+++ src/ai4cr/neural_network/cmn/rnn_concerns/calc_guess.cr
@@ -5,7 +5,33 @@ module Ai4cr
     module Cmn
       module RnnConcerns
         module CalcGuess
+
+          def outputs_guessed
+            li = synaptic_layer_indexes.last
+
+            mn_output_size = node_output_sizes[li]
+            time_col_indexes.map do |ti|
+              mini_net_set[li][ti].outputs_guessed
+            end
+          end
           
+          # steps for 'eval' aka 'guess':
+          def eval(input_set_given)
+            step_load_inputs(input_set_given)
+            step_calc_forward
+
+            # outputs_guessed
+            [[-10.0], [-20.0]] 
+          end
+
+          def step_load_inputs(input_set_given)
+            # TODO
+          end
+
+          def step_calc_forward
+            # TODO
+          end
+
         end
       end
     end
