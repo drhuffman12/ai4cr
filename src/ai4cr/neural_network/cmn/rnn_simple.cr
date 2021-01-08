c6b1f7dda451fbda1db6d1fb83ed8e5524e7c759
drhuffman12 <drhuffman12@gmail.com>
2021-01-07 21:20:03 -0500

    drhuffman12 For now, comment out RnnConcerns::TrainInSequence; TODO in next PR

--- src/ai4cr/neural_network/cmn/rnn_simple.cr
+++ src/ai4cr/neural_network/cmn/rnn_simple.cr
@@ -5,16 +5,13 @@ require "./rnn_concerns/props_and_inits.cr"
 require "./rnn_concerns/train_and_adjust.cr"
 require "./rnn_concerns/roll_ups.cr"
 require "./rnn_concerns/split_training_data.cr"
-require "./rnn_concerns/train_in_sequence.cr"
+# require "./rnn_concerns/train_in_sequence.cr" # TODO!
 
 module Ai4cr
   module NeuralNetwork
     module Cmn
       class RnnSimple
-        # EMPTY_1D_ARRAY_FLOAT64 = Array(Float64).new
-
-        # Simple RNN w/ inputs, hidden forward-feeding recurrent layer(s), and outputs
-
+        # Simple RNN w/ inputs, hidden forward-feeding recurrent layer(s), outputs, and some other params
         include JSON::Serializable
 
         include RnnConcerns::PropsAndInits
@@ -22,7 +19,7 @@ module Ai4cr
         include RnnConcerns::TrainAndAdjust
         include RnnConcerns::RollUps
         include RnnConcerns::SplitTrainingData
-        include RnnConcerns::TrainInSequence
+        # include RnnConcerns::TrainInSequence # TODO!
       end
     end
   end
