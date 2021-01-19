db726b960077ce4e761898f342fcec06b3945e1e
Daniel Huffman <drhuffman12@gmail.com>
2021-01-18 23:16:04 -0500

    drhuffman12/add_team_utils add ErrorStats

---
+++ spec/ai4cr/error_stats_spec.cr
@@ -0,0 +1,149 @@
+require "./../spec_helper"
+require "./../spectator_helper"
+
+Spectator.describe Ai4cr::ErrorStats do
+  let(given_history_size) { 8 }
+  let(error_stats) { Ai4cr::ErrorStats.new(given_history_size) }
+
+  let(expected_initial_distance) { -1.0 }
+
+  let(expected_initial_score) {
+    # Float64::MAX / (2.0)*4
+    # Math.sqrt(Float64::MAX) # 2.0**16
+    1.844674407370955e+19
+  }
+  let(expected_initial_history) {
+    [] of Float64
+  }
+  let(to_json) { error_stats.to_json }
+  let(expected_initial_json) { "{\"history_size\":8,\"distance\":-1.0,\"history\":[],\"score\":1.844674407370955e+19}" }
+  let(expected_later_json) { "{\"history_size\":8,\"distance\":10.0,\"history\":[10.0],\"score\":5.0}" }
+
+  describe "#initialize" do
+    context "has" do
+      it "given history_size" do
+        expect(error_stats.history_size).to eq(given_history_size)
+      end
+
+      it "initial distance of zero" do
+        expect(error_stats.distance).to eq(expected_initial_distance)
+      end
+
+      it "initial history of an empty Array(Float64)" do
+        expect(error_stats.history).to eq(expected_initial_history)
+      end
+
+      it "initial history of an empty Array(Float64)" do
+        expect(error_stats.score).to eq(expected_initial_score)
+      end
+    end
+
+    context "to_json" do
+      context "returns" do
+        it "expected_initial_json" do
+          expect(to_json).to eq(expected_initial_json)
+        end
+      end
+    end
+
+    context "from_json" do
+      context "when given error_stats.to_json" do
+        context "and re-exported to_json" do
+          it "returns json matching original converted to_json" do
+            expect(Ai4cr::ErrorStats.from_json(to_json).to_json).to eq(expected_initial_json)
+          end
+        end
+      end
+    end
+  end
+
+  describe "#distance(value)" do
+    let(given_value) { 10.0 }
+    it "sets 'distance' to given value" do
+      error_stats.distance = given_value
+
+      expect(error_stats.distance).to eq(given_value)
+    end
+
+    context "to_json" do
+      context "returns" do
+        it "expected_later_json" do
+          error_stats.distance = given_value
+
+          expect(to_json).to eq(expected_later_json)
+        end
+      end
+    end
+
+    context "from_json" do
+      context "when given error_stats.to_json" do
+        context "and re-exported to_json" do
+          it "returns json matching original converted to_json" do
+            error_stats.distance = given_value
+            
+            expect(Ai4cr::ErrorStats.from_json(to_json).to_json).to eq(expected_later_json)
+          end
+        end
+      end
+    end
+  end
+
+  describe "#plot_error_distance_history" do
+    let(given_values) { [4.0, 2.0, 3.0, 1.0, 0.5, 0.3, 0.1, 0.4, 0.01] }
+
+    let(expected_plot_first_4) { "▴▴▴▴" }
+    let(expected_plot_all) { "▴▴▴▄▂_▃▿" }
+
+    context "when given_values.size > history_size" do
+      it "given_values.size > history_size" do
+        expect(given_values.size).to be > error_stats.history_size
+      end
+
+      context "after loading less than history_size of given_values" do
+        before_each do
+          given_values[0..3].each do |value|
+            error_stats.distance = value
+          end
+        end
+
+        it "returns expected string" do
+          plot = error_stats.plot_error_distance_history(in_bw: true).clone
+          
+          # puts plot
+
+          expect(plot).to eq(expected_plot_first_4)
+        end
+
+        it "plot size is less than history_size" do
+          plot = error_stats.plot_error_distance_history(in_bw: true).clone
+          
+          # puts plot
+          expect(plot.size).to be < error_stats.history_size
+        end
+      end
+
+      context "after loading first all of given_values" do
+        before_each do
+          given_values.each do |value|
+            error_stats.distance = value
+          end
+        end
+
+        it "returns expected string" do
+          plot = error_stats.plot_error_distance_history(in_bw: true).clone
+          
+          # puts plot
+
+          expect(plot).to eq(expected_plot_all)
+        end
+
+        it "plot size maxes out at history_size" do
+          plot = error_stats.plot_error_distance_history(in_bw: true).clone
+          
+          # puts plot
+          expect(plot.size).to eq(error_stats.history_size)
+        end
+      end
+    end
+  end
+end
---
+++ src/ai4cr/error_stats.cr
@@ -0,0 +1,66 @@
+module Ai4cr
+  class ErrorStats
+    include ::JSON::Serializable
+
+    # TODO: Update other clases so as to use ErrorStats, e.g.: replace usage as shown...
+    #   error_distance_history_max   => history_size
+    #   error_distance               => distance
+    #   error_distance_history       => history
+    #   error_distance_history_score => score
+    
+    # Must init @score, so set it big enough but not too big (so ErrorStats works w/ to/from JSON)
+    INITIAL_SCORE = Math.sqrt(Math.sqrt(Math.sqrt(Math.sqrt(Float64::MAX))))
+    
+    getter history_size : Int32
+    getter distance : Float64
+    getter history : Array(Float64)
+    getter score : Float64
+
+    def initialize(@history_size = 10)
+      raise "Invalid size" if history_size < 0
+
+      @distance = -1.0
+      @history = Array(Float64).new
+
+      # lowest score is best; negatives are effectively invalid
+      @score = INITIAL_SCORE
+    end
+
+    def distance=(value)
+      raise "Invalid value" if value < 0.0
+      @distance = value
+      update_history
+
+      @distance
+    end
+
+    def plot_error_distance_history(
+      min = 0.0,
+      max = 1.0,
+      precision = 2.to_i8,
+      in_bw = false,
+      prefixed = false,
+      reversed = false
+    )
+      charter = AsciiBarCharter.new(min: min, max: max, precision: precision, in_bw: in_bw, inverted_colors: reversed)
+      charter.plot(history, prefixed)
+    end
+
+    private def update_history
+      if @history.size < @history_size  #- 1
+        # Array not 'full' yet, so add latest value to end
+        @history << @distance
+      else
+        # Array 'full', so rotate end to front and then put new value at last index
+        @history.rotate!
+        @history[-1] = @distance
+      end
+
+      @score = history.map_with_index do |e, i|
+        e / (2.0 ** (@history.size - i))
+      end.sum
+
+      @history
+    end
+  end
+end
