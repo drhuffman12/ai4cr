32262e9adb9ac19b4613fa778355384e05c044e7
Daniel Huffman <drhuffman12@gmail.com>
2021-04-10 04:32:09 -0400

    drhuffman12/add_team_utils_part_8 comment out RnnBiDi for now (seems to break tests for RnnSimple; not sure why

--- src/ai4cr/neural_network/rnn/rnn_bidi.cr
+++ src/ai4cr/neural_network/rnn/rnn_bidi.cr
@@ -1,12 +1,14 @@
-require "./rnn_simple.cr"
+# require "./rnn_simple.cr"
 
-module Ai4cr
-  module NeuralNetwork
-    module Rnn
-      class RnnBiDi < RnnSimple
-        # TODO: Implement Bi-directional RNN (i.e.: RnnSimple pulls from inputs and previous time column.)
-        # This class must also pull from next time column and mix them all together in subsequent hidden layers.
-      end
-    end
-  end
-end
+# module Ai4cr
+#   module NeuralNetwork
+#     module Rnn
+#       class RnnBiDi < RnnSimple
+#         # TODO: Implement Bi-directional RNN (i.e.: RnnSimple pulls from inputs and previous time column.)
+#         # This class must also pull from next time column and mix them all together in subsequent hidden layers.
+
+#         property birth_id : Int32 = -1
+#       end
+#     end
+#   end
+# end
