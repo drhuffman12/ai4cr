66da8e8a31b86778d560449938e5ffb080b97968
Daniel Huffman <drhuffman12@gmail.com>
2021-06-06 05:19:07 -0400

    drhuffman12/parallel_mini_net adjust state machine tests

--- spec/state_machine_spec.cr
+++ spec/state_machine_spec.cr
@@ -6,128 +6,245 @@ Spectator.describe SimpleStateMachine::StateMachine do
   context "with all defaults used (i.e.: no params specified)" do
     let(state_machine) { SimpleStateMachine::StateMachine.new }
 
+    let(states_expected) { SimpleStateMachine::StateMachine::DEFAULT_STATES }
+    let(state_current_expected) { SimpleStateMachine::StateMachine::STATE_NEW }
+    let(state_change_attempts_expected) {
+      [{forced: true, state_changed: true, state_from: :new, state_to: :new}]
+    }
+    let(allowed_paths_expected) {
+      {
+        :new       => [:init, :error, :reset],
+        :init      => [:ready, :error, :reset],
+        :ready     => [:start, :error, :reset],
+        :start     => [:running, :error, :reset],
+        :running   => [:success, :error, :reset],
+        :success   => [:done, :error, :reset],
+        :done      => [:ready, :error, :reset],
+        :error     => [:stopped, :error, :reset],
+        :stopped   => [:reset, :error, :reset],
+        :reset     => [:resetting, :error, :reset],
+        :resetting => [:ready, :error, :reset],
+      }
+    }
+
     describe "#initialize" do
       it "does not crash" do
         expect {
           SimpleStateMachine::StateMachine.new
         }.not_to raise_error
       end
+
+      it "starts with expected states" do
+        expect(state_machine.states).to eq(states_expected)
+      end
+
+      it "starts with expected state_current" do
+        expect(state_machine.state_current).to eq(state_current_expected)
+      end
+
+      it "starts with expected state_change_attempts" do
+        expect(state_machine.state_change_attempts).to eq(state_change_attempts_expected)
+      end
+
+      it "starts with expected allowed_paths" do
+        expect(state_machine.allowed_paths).to eq(allowed_paths_expected)
+      end
     end
 
-    describe "#next" do
+    describe "#init_paths" do
+      it "DEBUG" do
+        p! "#init_paths"
+        p! state_machine
+        expect(state_machine).not_to be_nil
+      end
+
       it "does not crash" do
+        state_machine = SimpleStateMachine::StateMachine.new
         expect {
-          state_machine.next
+          state_machine.init_paths
         }.not_to raise_error
       end
-      it "By default, it returns the time and current_state" do
-        state_before = state_machine.state_current
-        p! state_before
-        state_machine.next
-        state_after = state_machine.state_current
-        p! state_after
-        expect(state_before).to eq(SimpleStateMachine::StateMachine::STATE_NEW)
-        expect(state_after).to eq(SimpleStateMachine::StateMachine::STATE_INIT)
-      end
-
-      it "twice" do
-        state_before = state_machine.state_current
-        p! state_before
-        state_machine.next
-        state_mid = state_machine.state_current
-        state_machine.next
-        state_after = state_machine.state_current
-        p! state_after
-        expect(state_before).to eq(SimpleStateMachine::StateMachine::STATE_NEW)
-        expect(state_mid).to eq(SimpleStateMachine::StateMachine::STATE_INIT)
-        expect(state_after).to eq(SimpleStateMachine::StateMachine::STATE_READY)
-      end
     end
 
+    # describe "#next" do
+    #   it "DEBUG" do
+    #     p! "#next"
+    #     p! state_machine
+    #     expect(state_machine).not_to be_nil
+    #   end
+
+    #   it "does not crash" do
+    #     state_machine = SimpleStateMachine::StateMachine.new
+    #     state_machine.init_paths
+    #     expect {
+    #       state_machine.next
+    #     }.not_to raise_error # (SimpleStateMachine::StateFromException)
+    #   end
+    #   it "By default, it returns the time and current_state" do
+    #     state_before = state_machine.state_current.clone
+    #     p! state_before
+    #     state_machine.next
+    #     state_after = state_machine.state_current.clone
+    #     p! state_after
+
+    #     puts
+    #     p! "#next .. By default, it returns the time and current_state"
+    #     puts
+    #     p! state_machine
+    #     puts
+    #     p! state_machine.state_change_attempts
+    #     puts
+
+    #     expect(state_before).to eq(SimpleStateMachine::StateMachine::STATE_NEW)
+    #     expect(state_after).to eq(SimpleStateMachine::StateMachine::STATE_INIT)
+    #   end
+
+    #   it "twice" do
+    #     state_before = state_machine.state_current
+    #     p! state_before
+    #     state_machine.next
+    #     state_mid = state_machine.state_current
+    #     state_machine.next
+    #     state_after = state_machine.state_current
+    #     p! state_after
+    #     expect(state_before).to eq(SimpleStateMachine::StateMachine::STATE_NEW)
+    #     expect(state_mid).to eq(SimpleStateMachine::StateMachine::STATE_INIT)
+    #     expect(state_after).to eq(SimpleStateMachine::StateMachine::STATE_READY)
+    #   end
+    # end
+
     describe "#to_json" do
-      let(obj_to_json_expected) {
-        {
-          "states": [
-            "new",
-            "init",
-            "ready",
-            "start",
-            "running",
-            "error",
-            "stopped",
-            "reset",
-            "resetting",
-            "success",
-            "done",
-          ],
-          "state_current": "new",
-          "history_size":  10,
-          "history":       [
-            "new",
-          ],
-          "allowed_paths": {
-            "new": [
-              "init",
-              "error",
-              "reset",
-            ],
-            "init": [
-              "ready",
-              "error",
-              "reset",
-            ],
-            "ready": [
-              "start",
-              "error",
-              "reset",
-            ],
-            "start": [
-              "running",
-              "error",
-              "reset",
-            ],
-            "running": [
-              "success",
-              "error",
-              "reset",
-            ],
-            "success": [
-              "done",
-              "error",
-              "reset",
-            ],
-            "done": [
-              "ready",
-              "error",
-              "reset",
-            ],
-            "error": [
-              "stopped",
-              "error",
-              "reset",
-            ],
-            "stopped": [
-              "reset",
-              "error",
-              "reset",
-            ],
-            "reset": [
-              "resetting",
-              "error",
-              "reset",
-            ],
-            "resetting": [
-              "ready",
-              "error",
-              "reset",
-            ],
-          },
-          "use_default_paths": true,
-        }.to_json
-      }
-      it "returns expected json" do
-        expect(state_machine.to_json).to eq(obj_to_json_expected)
+      # let(obj_to_json_expected) {
+      #   {
+      #     "states": [
+      #       "new",
+      #       "init",
+      #       "ready",
+      #       "start",
+      #       "running",
+      #       "error",
+      #       "stopped",
+      #       "reset",
+      #       "resetting",
+      #       "success",
+      #       "done",
+      #     ],
+      #     "state_current": "new",
+      #     "history_size":  10,
+      #     "history":       [
+      #       "new",
+      #     ],
+      #     "allowed_paths": {
+      #       "new": [
+      #         "init",
+      #         "error",
+      #         "reset",
+      #       ],
+      #       "init": [
+      #         "ready",
+      #         "error",
+      #         "reset",
+      #       ],
+      #       "ready": [
+      #         "start",
+      #         "error",
+      #         "reset",
+      #       ],
+      #       "start": [
+      #         "running",
+      #         "error",
+      #         "reset",
+      #       ],
+      #       "running": [
+      #         "success",
+      #         "error",
+      #         "reset",
+      #       ],
+      #       "success": [
+      #         "done",
+      #         "error",
+      #         "reset",
+      #       ],
+      #       "done": [
+      #         "ready",
+      #         "error",
+      #         "reset",
+      #       ],
+      #       "error": [
+      #         "stopped",
+      #         "error",
+      #         "reset",
+      #       ],
+      #       "stopped": [
+      #         "reset",
+      #         "error",
+      #         "reset",
+      #       ],
+      #       "reset": [
+      #         "resetting",
+      #         "error",
+      #         "reset",
+      #       ],
+      #       "resetting": [
+      #         "ready",
+      #         "error",
+      #         "reset",
+      #       ],
+      #     },
+      #   }.to_json
+      # }
+      context "returns json with expected keys" do
+        let(json_expected) {
+          # TODO: Remove this
+          <<-JSON
+          {"states":["new","init","ready","start","running","error","stopped","reset","resetting","success","done"],"state_current":"new","history_size":10,"allowed_paths":{"new":["init","error","reset"],"init":["ready","error","reset"],"ready":["start","error","reset"],"start":["running","error","reset"],"running":["success","error","reset"],"success":["done","error","reset"],"done":["ready","error","reset"],"error":["stopped","error","reset"],"stopped":["reset","error","reset"],"reset":["resetting","error","reset"],"resetting":["ready","error","reset"]},"state_change_attempts":[{"forced":true,"state_changed":true,"state_from":"new","state_to":"new"}]}
+          JSON
+        }
+        it "returns expected json" do
+          puts "state_machine.to_json:"
+          puts state_machine.to_json
+
+          expect(state_machine.to_json).to eq(json_expected)
+        end
+
+        let(json_data) { JSON.parse(state_machine.to_json) }
+
+        let(json_expected_states) {
+          ["new", "init", "ready", "start", "running", "error", "stopped", "reset", "resetting", "success", "done"]
+        }
+        it "states" do
+          expect(json_data["states"]).to eq(json_expected_states)
+        end
+
+        let(json_expected_state_current) { "new" }
+        it "state_current" do
+          expect(json_data["state_current"]).to eq(json_expected_state_current)
+        end
+
+        let(json_expected_history_size) { 10 }
+        it "history_size" do
+          expect(json_data["history_size"]).to eq(json_expected_history_size)
+        end
+
+        let(json_expected_allowed_paths) {
+          {"new" => ["init", "error", "reset"], "init" => ["ready", "error", "reset"], "ready" => ["start", "error", "reset"], "start" => ["running", "error", "reset"], "running" => ["success", "error", "reset"], "success" => ["done", "error", "reset"], "done" => ["ready", "error", "reset"], "error" => ["stopped", "error", "reset"], "stopped" => ["reset", "error", "reset"], "reset" => ["resetting", "error", "reset"], "resetting" => ["ready", "error", "reset"]}
+        }
+        it "allowed_paths" do
+          expect(json_data["allowed_paths"]).to eq(json_expected_allowed_paths)
+        end
+
+        let(json_expected_state_change_attempts) {
+          [{"forced" => true, "state_changed" => true, "state_from" => "new", "state_to" => "new"}]
+        }
+        it "state_change_attempts" do
+          expect(json_data["state_change_attempts"]).to eq(json_expected_state_change_attempts)
+        end
       end
+      # context "returns json with expected value(s) for key" do
+      #   it ":foo" do
+      #     expect(state_machine.to_json).to eq(obj_to_json_expected)
+      #   end
+      # end
     end
   end
 
@@ -151,66 +268,72 @@ Spectator.describe SimpleStateMachine::StateMachine do
     end
 
     describe "#next" do
-      it "does not crash" do
-        expect {
-          state_machine.next
-        }.not_to raise_error
+      context "when no paths set" do
+        it "raises" do
+          expect {
+            state_machine.next
+          }.not_to raise_error # (SimpleStateMachine::StateNextMissingException, "Paths not defined.")
+        end
       end
-      it "By default, it returns the time and current_state" do
-        # expect(Time).to receive(:utc)
-        # expect(state_machine).to receive(:goto)
-        state_before = state_machine.state_current
-        p! state_before
-        state_machine.next
-        state_after = state_machine.state_current
-        p! state_after
-        expect(state_before).to eq(state0)
-        expect(state_after).to eq(state1)
-      end
+      # it "does not crash" do
+      #   expect {
+      #     state_machine.next
+      #   }.not_to raise_error
+      # end
+      # it "By default, it returns the time and current_state" do
+      #   # expect(Time).to receive(:utc)
+      #   # expect(state_machine).to receive(:goto)
+      #   state_before = state_machine.state_current
+      #   p! state_before
+      #   state_machine.next
+      #   state_after = state_machine.state_current
+      #   p! state_after
+      #   expect(state_before).to eq(state0)
+      #   expect(state_after).to eq(state1)
+      # end
     end
 
-    describe "#to_json" do
-      let(obj_to_json_expected) {
-        {
-          "states": [
-            state0,
-            state1,
-            state2,
-            state3,
-          ],
-          "state_current": state0,
-          "history_size":  10,
-          "history":       [
-            state0,
-          ],
-          "allowed_paths": {
-            state0 => [
-              state1,
-              state2,
-              state3,
-            ],
-            state1 => [
-              state0,
-              state2,
-              state3,
-            ],
-            state2 => [
-              state0,
-              state1,
-              state3,
-            ],
-            state3 => [
-              state0,
-              state1,
-              state2,
-            ],
-          },
-          "use_default_paths": true,
-        }.to_json
-      }
-      it "returns expected json" do
-        expect(state_machine.to_json).to eq(obj_to_json_expected)
-      end
-    end
+    # describe "#to_json" do
+    #   let(obj_to_json_expected) {
+    #     {
+    #       "states": [
+    #         state0,
+    #         state1,
+    #         state2,
+    #         state3,
+    #       ],
+    #       "state_current": state0,
+    #       "history_size":  10,
+    #       "history":       [
+    #         state0,
+    #       ],
+    #       "allowed_paths": {
+    #         state0 => [
+    #           state1,
+    #           state2,
+    #           state3,
+    #         ],
+    #         state1 => [
+    #           state0,
+    #           state2,
+    #           state3,
+    #         ],
+    #         state2 => [
+    #           state0,
+    #           state1,
+    #           state3,
+    #         ],
+    #         state3 => [
+    #           state0,
+    #           state1,
+    #           state2,
+    #         ],
+    #       },
+    #     }.to_json
+    #   }
+    #   it "returns expected json" do
+    #     expect(state_machine.to_json).to eq(obj_to_json_expected)
+    #   end
+    # end
   end
 end
--- src/state_machine.cr
+++ src/state_machine.cr
@@ -52,17 +52,59 @@ module SimpleStateMachine
       STATE_DONE,
     ]
 
+    # alias StateChangedLatest = { state_changed: true, state_from: :some_state, state_to: :other_state }.class
+    alias StateChangedAttempt = NamedTuple(forced: Bool, state_changed: Bool, state_from: Symbol, state_to: Symbol)
+
     getter states : Array(Symbol)
     getter state_current : Symbol
     getter history_size : Int32
-    getter history : Array(Symbol)
+    # getter history : Array(Symbol)
     getter allowed_paths : AllowedPaths
+    getter state_change_attempts : Array(StateChangedAttempt)
 
     def initialize(@states : StateList = DEFAULT_STATES, @history_size = 10)
       @state_current = @states.first
-      @history = [@state_current]
+      # @history = [@state_current]
       @allowed_paths = AllowedPaths.new
+      @state_change_attempts = Array(StateChangedAttempt).new
       init_paths
+    end
+
+    def init_paths
+      # Feel free to modify in Sub-classes
+      @states = DEFAULT_STATES if (@states.empty?)
+
+      if (@states.sort == DEFAULT_STATES.sort)
+        # Use a typical new/run/success/error/reset state-flow:
+
+        # 'Happy StateList (w/ first next state as a 'Happy State'):
+        add_path(state_from: STATE_NEW, states_to: [STATE_INIT, STATE_ERROR, STATE_RESET], restriced_states: true)
+        add_path(state_from: STATE_INIT, states_to: [STATE_READY, STATE_ERROR, STATE_RESET], restriced_states: true)
+        add_path(state_from: STATE_READY, states_to: [STATE_START, STATE_ERROR, STATE_RESET], restriced_states: true)
+        add_path(state_from: STATE_START, states_to: [STATE_RUNNING, STATE_ERROR, STATE_RESET], restriced_states: true)
+        add_path(state_from: STATE_RUNNING, states_to: [STATE_SUCCESS, STATE_ERROR, STATE_RESET], restriced_states: true)
+
+        add_path(state_from: STATE_SUCCESS, states_to: [STATE_DONE, STATE_ERROR, STATE_RESET], restriced_states: true)
+        add_path(state_from: STATE_DONE, states_to: [STATE_READY, STATE_ERROR, STATE_RESET], restriced_states: true)
+
+        # Un-Happy Paths (STATE_ERROR)
+        add_path(state_from: STATE_ERROR, states_to: [STATE_STOPPED, STATE_ERROR, STATE_RESET], restriced_states: true)
+        add_path(state_from: STATE_STOPPED, states_to: [STATE_RESET, STATE_ERROR, STATE_RESET], restriced_states: true)
+
+        # Un-Happy Paths (STATE_RESET)
+        add_path(state_from: STATE_RESET, states_to: [STATE_RESETTING, STATE_ERROR, STATE_RESET], restriced_states: true)
+        add_path(state_from: STATE_RESETTING, states_to: [STATE_READY, STATE_ERROR, STATE_RESET], restriced_states: true)
+      else
+        # Use a any-to-any (except self) state-flow:
+        @states.map do |state_from|
+          states_to = @states - [state_from]
+
+          # @allowed_paths.merge!({state_from => states_to})
+          add_path(state_from: state_from, states_to: states_to, restriced_states: false)
+        end
+      end
+
+      goto(next_state: @allowed_paths.keys.first, forced: true) unless @allowed_paths.keys.empty?
     end
 
     def add_path(state_from : State, states_to : StateList, restriced_states = false)
@@ -97,58 +139,55 @@ module SimpleStateMachine
     end
 
     def next
-      raise "No Paths Initialized" if allowed_paths_undefined?(@state_current)
-      raise "No Paths Set" if allowed_paths_empty?(@state_current)
-
-      goto(@allowed_paths[@state_current].first)
-    end
-
-    def goto(next_state : Symbol)
-      @state_current = if allowed_next_states.includes?(next_state)
-                         @history << @state_current
-                         @history.pop if @history.size > @history_size
-                         next_state
-                       else
-                         @state_current
-                       end
-      callbacks_for(@state_current)
-    end
-
-    def init_paths
-      # Feel free to modify in Sub-classes
-      if (@states.sort == DEFAULT_STATES.sort)
-        # Use a typical new/run/success/error/reset state-flow:
-
-        # 'Happy StateList (w/ first next state as a 'Happy State'):
-        add_path(state_from: STATE_NEW, states_to: [STATE_INIT, STATE_ERROR, STATE_RESET], restriced_states: true)
-        add_path(state_from: STATE_INIT, states_to: [STATE_READY, STATE_ERROR, STATE_RESET], restriced_states: true)
-        add_path(state_from: STATE_READY, states_to: [STATE_START, STATE_ERROR, STATE_RESET], restriced_states: true)
-        add_path(state_from: STATE_START, states_to: [STATE_RUNNING, STATE_ERROR, STATE_RESET], restriced_states: true)
-        add_path(state_from: STATE_RUNNING, states_to: [STATE_SUCCESS, STATE_ERROR, STATE_RESET], restriced_states: true)
-
-        add_path(state_from: STATE_SUCCESS, states_to: [STATE_DONE, STATE_ERROR, STATE_RESET], restriced_states: true)
-        add_path(state_from: STATE_DONE, states_to: [STATE_READY, STATE_ERROR, STATE_RESET], restriced_states: true)
-
-        # Un-Happy Paths (STATE_ERROR)
-        add_path(state_from: STATE_ERROR, states_to: [STATE_STOPPED, STATE_ERROR, STATE_RESET], restriced_states: true)
-        add_path(state_from: STATE_STOPPED, states_to: [STATE_RESET, STATE_ERROR, STATE_RESET], restriced_states: true)
-
-        # Un-Happy Paths (STATE_RESET)
-        add_path(state_from: STATE_RESET, states_to: [STATE_RESETTING, STATE_ERROR, STATE_RESET], restriced_states: true)
-        add_path(state_from: STATE_RESETTING, states_to: [STATE_READY, STATE_ERROR, STATE_RESET], restriced_states: true)
+      # raise StateNextMissingException.new("Paths not defined.") if allowed_paths_undefined?(@state_current)
+      # raise StateNextMissingException.new("Paths missing.") if allowed_paths_empty?(@state_current)
+      if allowed_paths_undefined?(@state_current) || allowed_paths_empty?(@state_current)
+        @state_change_attempts << {forced: false, state_changed: false, state_from: @state_current, state_to: @state_current}
       else
-        # Use a any-to-any (except self) state-flow:
-        @states.map do |state_from|
-          states_to = @states - [state_from]
-
-          @allowed_paths.merge!({state_from => states_to})
-        end
+        goto(@allowed_paths[@state_current].first)
       end
+    end
+
+    def append_state_change_attempts(attempted)
+      @state_change_attempts << attempted
+      @state_change_attempts = @state_change_attempts[0..history_size - 1] if @state_change_attempts.size > history_size
+    end
+
+    def goto(next_state : Symbol, forced = false)
+      if forced
+        state_from = @state_current.clone
+        state_to = next_state
+        @state_current = state_to
+        callbacks_for(@state_current)
+        append_state_change_attempts({forced: true, state_changed: true, state_from: state_from, state_to: state_to})
+      else
+        state_from = @state_current.clone
+        state_to = next_state
+        @state_current = state_to
+        succeded = callbacks_for(@state_current)
+        @state_current = state_from unless succeded
+        append_state_change_attempts({forced: false, state_changed: succeded, state_from: state_from, state_to: state_to})
+      end
+
+      # state_changed = false
+      # state_before = @state_current
+
+      # @state_current = if allowed_next_states.includes?(next_state)
+      #                    @history << @state_current
+      #                    @history.pop if @history.size > @history_size
+      #                    next_state
+      #                  else
+      #                    @state_current
+      #                  end
+      # state_changed = callbacks_for(@state_current)
+      # state_after = @state_current
+      # @state_change_attempts = { forced: false, state_changed: state_changed, state_from: state_before, state_to: state_after }
     end
 
     def callbacks_for(when_state)
       # Sub-class and replace below with your per-state call-backs
       puts "callbacks_for(:#{when_state}) @ #{Time.utc}"
+      true # if callbacks succeeded
     end
   end
 end
